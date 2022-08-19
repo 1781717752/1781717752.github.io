dfcd0817fcb0652c1dbfd8432196c72d50aa2eb4
1781717752 <1781717752@qq.com>
2022-08-19 12:19:36 +0800

    Site updated: 2022-08-19 12:19:35

--- js/music.js
+++ js/music.js
@@ -1,22 +1,24 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    autoplay: false,
-    loop: 'all',
-    volume: 0.7,
-    listFolded: true,//列表默认折叠
-    listMaxHeight: 60,//列表最大高度
+    container: document.getElementById('player'),
+    listFolded: false,//列表默认折叠
+    listMaxHeight: 90,//列表最大高度
+    lrcType: 3, //此为歌词格式，没有歌词可以直接删掉这一行
     audio: [
         {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
+            name: '花の塔',//歌曲名
+            artist: 'さユり',//歌曲作者
+            url: 'http://music.163.com/song/media/outer/url?id=1956534872.mp3',//歌曲链接
+            cover: 'https://p2.music.126.net/fS_5RbP_4qg-RYreqp2tGg==/109951167558017839.jpg?param=130y130',//歌曲图片
+            lrc: 'https://moechun.fun/music/lrc/さユり - 花の塔.lrc',//歌词
+            theme: '#ebd0c2'
         },
         {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'url2.mp3',
-            cover: 'cover2.jpg',
+            name: '花の塔',
+            artist: 'さユり',
+            url: 'http://music.163.com/song/media/outer/url?id=1956534872.mp3',
+            cover: 'https://p2.music.126.net/fS_5RbP_4qg-RYreqp2tGg==/109951167558017839.jpg?param=130y130',
+            lrc: 'https://moechun.fun/music/lrc/さユり - 花の塔.lrc'
+            theme: '#46718b'
         }
     ]
 });
